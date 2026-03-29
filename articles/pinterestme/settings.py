a2688290730d0c59354bda859b92dc8ec808602e
nastea <anastasiaanasta61@gmail.com>
2026-03-29 18:32:35 +0500

    Change DEBUG setting to False
    Updated DEBUG setting to False for production.

--- articles/pinterestme/settings.py
+++ articles/pinterestme/settings.py
@@ -18,14 +18,12 @@ import pytz
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
+
 SECRET_KEY = 'django-insecure-zqp+_&_(hose_#3wmicj^5wmm&e6u3g8i)ul*mi8^*x!9jl=ji'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS =  ['django-site-articles.onrender.com', 'localhost', '127.0.0.1']
 
